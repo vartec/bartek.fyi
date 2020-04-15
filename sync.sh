665203ce8091a462766bf67402fece24d79e35fc
Bartek Ogryczak <bartek@gmail.com>
2020-04-14 19:40:12 -0700

    one font to rule them all

--- sync.sh
+++ sync.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-aws s3 sync ~/bartek.fyi/ s3://bartek.fyi/ --exclude ".git*" --acl 'public-read'
+aws s3 sync ~/bartek.fyi/ s3://bartek.fyi/ --exclude "*" \
+--include "*.html" --include "*.css" --include "*.pdf" --acl "public-read"
+    
