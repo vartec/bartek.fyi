3621e5e3c0469c4ef753e49b656a274a2a133526
Bartek Ogryczak <b.ogryczak@gmail.com>
2023-03-06 19:51:46 -0800

    nalewka

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 aws s3 sync ~/bartek.fyi/ s3://bartek.fyi/ --exclude "*" \
 --include "*.html" --include "*.css" --include "*.pdf" --acl "public-read"
+aws s3 sync ~/bartek.fyi/nalewka/ s3://bartek.fyi/nalewka/ --exclude "*" \
+--include "*.html" --include "*.css" --include "*.jpg" --include "*.gif" --acl "public-read"
 aws cloudfront create-invalidation --distribution-id EAPPH9FF15TM1 --paths '/*'
     
